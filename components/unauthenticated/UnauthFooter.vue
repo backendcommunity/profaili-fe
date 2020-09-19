2d6d771cca4149e1ea8e2ee82ff41280fd090629
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2020-09-19 01:10:41 +0100

    Finished home desktop

---
+++ components/unauthenticated/UnauthFooter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="unauth-footer">
+    <p>© Copyright {{ name }} 2020, All rights reserved.</p>
+    <div class="icons">
+      <a href="#"><img :src="fb"/></a>
+      <a href="#"><img :src="twitter"/></a>
+      <a href="#"><img :src="linkedin"/></a>
+    </div>
+  </div>
+</template>
+<script>
+import fb from "~/assets/icons/facebook.svg";
+import twitter from "~/assets/icons/twitter.svg";
+import linkedin from "~/assets/icons/linkedin.svg";
+export default {
+  data: function() {
+    return {
+      name: process.env.APP_NAME || "Profaili",
+      fb,
+      twitter,
+      linkedin
+    };
+  }
+};
+</script>
+<style lang="sass" scoped>
+.unauth-footer
+  display: flex
+  justify-content: space-between
+  width: 100%
+  margin: 30px 0
+  *
+    color: #041836
+  p
+    margin: 0
+    font-weight: 500
+  .icons
+    a
+      margin: 0 8px
+</style>
---
+++ layouts/unauthenticated.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <UnauthNav />
+    <Nuxt />
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: process.env.APP_NAME || "Profaili",
+      link: [
+        {
+          rel: "stylesheet",
+          href:
+            "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style>
+* {
+  font-family: "Montserrat", sans-serif;
+  box-sizing: border-box
+}
+.e-container {
+  width: 100%;
+  padding: 0 120px;
+  margin: 0;
+}
+</style>
